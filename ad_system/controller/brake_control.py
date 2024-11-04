2ae3d04ca2edf323d50c9b1d18860c49a322a4c4
jusung <kimjusung2109@naver.com>
2024-11-04 21:28:04 +0900

    modify brake_control.py
    1. brake 값 정규화

--- ad_system/controller/brake_control.py
+++ ad_system/controller/brake_control.py
@@ -1,9 +1,11 @@
 
-def calc_brake_command(desired_acceleration):
+def calc_brake_command(desired_acceleration, max_deceleration):
 
     if desired_acceleration >= 0:
         brake_cmd = 0
     else:
-        brake_cmd = -desired_acceleration
+        brake_cmd = -desired_acceleration / 7 
+
+
 
     return brake_cmd
