024cdc36fbd9ce38c34d57b41a2db68d5646d0f8
jusung <kimjusung2109@naver.com>
2024-11-04 21:28:43 +0900

    modify engine_control.py
    1. throttle 값 정규화

--- ad_system/controller/engine_control.py
+++ ad_system/controller/engine_control.py
@@ -1,8 +1,12 @@
 
-def calc_engine_control_command(desired_acceleration):
+def calc_engine_control_command(desired_acceleration, max_acceleration):
 
-    if desired_acceleration >= 0:
-        throttle_cmd = desired_acceleration
+    if desired_acceleration > 0:
+        throttle_cmd = desired_acceleration / max_acceleration 
+        if throttle_cmd < 0.65: 
+            throttle_cmd = 2 * throttle_cmd
+            if throttle_cmd > 0.65: 
+                throttle_cmd = 0.65
     else:
         throttle_cmd = 0
 
